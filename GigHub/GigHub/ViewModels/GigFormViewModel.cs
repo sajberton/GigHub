5fcb9cbc6b4dec063a46aa4cdb0852531680b030
tonimihajlovski <tonimihajlovski@yahoo.co.uk>
2019-03-09 15:35:28 +0100

    Added Server Side Validation, DateTime Changed to function in viewModel

--- GigHub/GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GigHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,23 @@ namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
-        {   get
-            {
-                return DateTime.Parse(String.Format("{0} {1}", Date, Time));
-            }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(String.Format("{0} {1}", Date, Time));
         }
     }
 }
