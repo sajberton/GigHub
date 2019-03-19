b3834a0885ff75bfce074ef07f09d57dfb1fb7ed
tonimihajlovski <tonimihajlovski@yahoo.co.uk>
2019-03-19 21:59:51 +0100

    Genre icluded in Home-Index, Layout in Index changed

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -19,9 +19,10 @@ namespace GigHub.Controllers
 
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.
-                Include(g => g.Artist).
-                Where(g => g.DateTime > DateTime.Now);
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
         }
