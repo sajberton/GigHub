f14c989ab8bad31c86a78fd89af23f29e77b5237
tonimihajlovski <tonimihajlovski@yahoo.co.uk>
2019-06-03 22:40:58 +0200

    Notification method for Canceled and Modified in Notification Controler

--- GigHub/GigHub/Models/Notification.cs
+++ GigHub/GigHub/Models/Notification.cs
@@ -11,8 +11,8 @@ namespace GigHub.Models
         public int Id { get; private set; }
         public DateTime DateTime { get; private set; }
         public NotificationType Type { get; private set; }
-        public DateTime? OriginalDateTime { get; set; }
-        public string OriginalVenue { get; set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVenue { get; private set; }
         
         [Required]
         public Gig Gig { get; private set; }
@@ -21,11 +21,32 @@ namespace GigHub.Models
         {
         }
 
-        public Notification(NotificationType type, Gig gig)
+        private Notification(NotificationType type, Gig gig)
         {
             Type = type;
             Gig = gig ?? throw new ArgumentNullException("gig");
             DateTime = DateTime.Now;
+        }
+
+        public static Notification GigCreated(Gig gig)
+        {
+            return new Notification(NotificationType.GigCreated, gig); 
+        }
+
+        public static Notification GigUpdated(Gig newGig, DateTime originalDateTime, string originalVenue)
+        {
+            var notification = new Notification(NotificationType.GigUpdated, newGig)
+            {
+                OriginalDateTime = originalDateTime,
+                OriginalVenue = originalVenue
+            };
+
+            return notification;
+        }
+
+        public static Notification GigCanceled(Gig gig)
+        {
+            return new Notification(NotificationType.GigCanceled, gig);
         }
     }
 }
