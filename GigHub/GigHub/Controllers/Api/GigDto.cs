a1e85244fc908c6afba3a113a94c09609f7f0ee9
tonimihajlovski <tonimihajlovski@yahoo.co.uk>
2019-06-08 15:57:06 +0200

    Creating Dtos

---
+++ GigHub/GigHub/Controllers/Api/GigDto.cs
@@ -0,0 +1,19 @@
+﻿using GigHub.Models;
+using System;
+
+
+namespace GigHub.Controllers.Api
+{
+    public class GigDto
+    {
+        public int Id { get; set; }
+        public bool IsCanceled { get; set; }
+        public UserDto Artist { get; set; }
+        public string ArtistId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public GenreDto Genre { get; set; }
+        public byte GenreId { get; set; }
+
+    }
+}
